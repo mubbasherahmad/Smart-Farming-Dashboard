0e6850180da24e8bc7bbb7b0c2a41db6359c3f19
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-16 22:01:32 +0500

    APP v5

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 //export const API_BASE = process.env.REACT_APP_API_URL || "http://13.238.116.205:5001/api";
 
 
-export const API_BASE = 'http://54.206.54.119:5001';
+export const API_BASE = 'http://54.206.54.119:5001/api';
 //hello12ww2wwe
