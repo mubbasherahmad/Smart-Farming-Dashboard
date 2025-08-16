20e21dc145fdcbc3d97543e3f2842f3a220f8fdd
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-16 13:30:43 +0500

    ISFD-10 Delete Schedule (Delete)

--- backend/routes/irrigationRoutes.js
+++ backend/routes/irrigationRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 const { irrigationScheduleValidator } = require('../middleware/irrigationValidator');
-const { createIrrigationSchedule,getIrrigationSchedules,updateIrrigationSchedule } = require('../controllers/irrigationController');
+const { createIrrigationSchedule,getIrrigationSchedules,updateIrrigationSchedule,deleteIrrigationSchedule } = require('../controllers/irrigationController');
 
 
 const router = express.Router();
 
 // Create new irrigation schedule
 router.post('/schedules', protect, irrigationScheduleValidator, createIrrigationSchedule);
-router.get("/schedules", protect, getIrrigationSchedules)
-router.put("/schedules/:id", protect, irrigationScheduleValidator, updateIrrigationSchedule);;
-
+router.get("/schedules", protect, getIrrigationSchedules);
+router.put("/schedules/:id", protect, irrigationScheduleValidator, updateIrrigationSchedule);
+router.delete("/schedules/:id", protect, deleteIrrigationSchedule);
 module.exports = router;
