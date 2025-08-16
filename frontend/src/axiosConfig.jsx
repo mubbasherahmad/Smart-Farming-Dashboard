568b24c2c747796a04c7b33b541215b59988c84b
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-16 22:27:00 +0500

    APP v5

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://54.206.54.119:5001', // local
   headers: { 'Content-Type': 'application/json' },
 });
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axiosInstance from '../axiosConfig';
+import axiosInstance from '../axiosInstance';
 
 const Register = () => {
   const [formData, setFormData] = useState({ name: '', email: '', password: '' });
