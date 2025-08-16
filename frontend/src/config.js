a82673dafd4a9d2135168d98ba4b5df2d4ba5cb8
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-16 16:41:30 +0500

    Instance running v5

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.REACT_APP_API_URL || "http://3.107.174.127:5001/api";
+export const API_BASE = process.env.REACT_APP_API_URL || "http://13.238.116.205:5001/api";
 
 
 
