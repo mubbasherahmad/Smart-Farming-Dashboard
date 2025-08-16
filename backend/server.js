23cb3989ad7e0908bd12ede124bdff17c1bc6a89
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-16 19:41:15 +0500

    APP

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const allowedOrigins = [
   "http://localhost:3000",           // dev frontend
-  "http://13.210.133.33:5001"        // deployed frontend
+  "http://13.238.116.205:5001"        // deployed frontend
 ];
 dotenv.config();
 
